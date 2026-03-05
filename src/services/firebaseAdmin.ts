edffc4f0386c065cb908aaf7e075344e132230da
g3support <support@g3fashion.com>
2026-03-06 00:40:38 +0530

    Update firebaseAdmin.ts

--- src/services/firebaseAdmin.ts
+++ src/services/firebaseAdmin.ts
@@ -1,18 +1,11 @@
 import admin from "firebase-admin";
 
 if (!admin.apps.length) {
-
   admin.initializeApp({
     credential: admin.credential.applicationDefault(),
     storageBucket: "gen-lang-client-00398625-33f08.firebasestorage.app"
   });
-
 }
 
 export const db = admin.firestore();
-
-<<<<<<< HEAD
 export const bucket = admin.storage().bucket();
-=======
-export const bucket = admin.storage().bucket();
->>>>>>> ae0906bef3bd040013a384b0623194687de20a5f
